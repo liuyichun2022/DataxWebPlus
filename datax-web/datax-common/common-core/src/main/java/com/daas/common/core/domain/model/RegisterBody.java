9387316ba057567eb762a8b7a91a3755912ffafe
liuyichun <liu_yichun@163.com>
2024-02-23 22:23:42 +0800

    基础组件封装

---
+++ datax-web/datax-common/common-core/src/main/java/com/daas/common/core/domain/model/RegisterBody.java
@@ -0,0 +1,34 @@
+package com.daas.common.core.domain.model;
+
+import com.daas.common.core.constant.UserConstants;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * 用户注册对象
+ *
+ * @author Lion Li
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class RegisterBody extends LoginBody {
+
+    /**
+     * 用户名
+     */
+    @NotBlank(message = "{user.username.not.blank}")
+    @Length(min = UserConstants.USERNAME_MIN_LENGTH, max = UserConstants.USERNAME_MAX_LENGTH, message = "{user.username.length.valid}")
+    private String username;
+
+    /**
+     * 用户密码
+     */
+    @NotBlank(message = "{user.password.not.blank}")
+    @Length(min = UserConstants.PASSWORD_MIN_LENGTH, max = UserConstants.PASSWORD_MAX_LENGTH, message = "{user.password.length.valid}")
+    private String password;
+
+    private String userType;
+
+}
